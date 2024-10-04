cff0d057b80bf2cc9ce13e7b8d5da8883b9d9c91
GODoal <godoal@hotmail.com>
2024-10-04 13:23:09 +0100

    Update audio.skai to python 3 compatible v1.1.0

--- plugin.audio.skai/default.py
+++ plugin.audio.skai/default.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Version 1.0.2 (03/03/2020)
+# Version 1.1.0 (04/10/2024)
 # SKAI RADIO
 # Greek News Channel XBMC addon
 # By GODoal
@@ -21,93 +21,68 @@
 import os
 import sys
 import xbmc
-import urllib
-import urllib2
+import urllib.request
 import re
 import xbmcplugin
 import xbmcgui
 import xbmcaddon
 import socket
-import HTMLParser
+from html.parser import HTMLParser
 import json
 __settings__ = xbmcaddon.Addon(id='plugin.audio.skai')
-__language__ = __settings__.getLocalizedString
 fanart = os.path.join(__settings__.getAddonInfo('path'),'fanart.jpg')
 BaseURL='http://www.skairadio.gr'
 
 #Load user settings
-timeout = int(__settings__.getSetting("socket_timeout"))
-socket.setdefaulttimeout(timeout)
+socket.setdefaulttimeout(15)
 
 #Index Menu
 def INDEX(url):
-	addLink("Live Stream","http://liveradio.skai.gr/skaihd/skai/playlist.m3u8",os.path.join(__settings__.getAddonInfo('path'),'resources','images','latest.png'))
+	addLink("Live Stream","http://skai.live24.gr/skai1003",os.path.join(__settings__.getAddonInfo('path'),'resources','images','latest.png'))
 	for i in range(1,3):
-	  req=urllib2.Request('http://www.skairadio.gr/shows?page='+str(i-1))
+	  req=urllib.request.Request('http://www.skairadio.gr/shows?page='+str(i-1))
 	  req.add_header('Accept', '*/*')
 	  req.add_header('Connection', 'keep-alive')
 	  req.add_header('Referer', 'http://www.skairadio.gr/shows?page='+str(i-1))
 	  req.add_header('Origin', BaseURL)
 	  req.add_header('Connection', 'keep-alive')
 	  req.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) XBMC Multimedia System')
-	  response = urllib2.urlopen(req)
+	  response = urllib.request.urlopen(req)
 	  link=response.read()
-	  response.close()
 	  link=normalize_link(link)
+	  response.close()
 	  block=re.compile('<section class="mb-5">(.+?)pagination').findall(link)
-	  match=re.compile('<a href="(.+?)">(.+?)src="(.+?)" alt="(.+?)font-weight-normal">(.+?)</h3>(.+?)<span class="d-block">').findall(block[0])
-	  for ep_url, buffer1, ep_image, buffer2, ep_title, buffer3 in match:
+	  match=re.compile('<a href="(.+?)">(.+?)src="(.+?)" alt="(.+?)" typeof="(.+?)</h3>(.+?)<span class\="d-block">(.+?)</span>').findall(block[0])
+	  for ep_url, buffer1, ep_image, ep_title, buffer2, buffer3, buffer4 in match:
 	    addDir(ep_title.replace(' &#039;','\''),BaseURL+ep_url,1,ep_image.split('?')[0])
-	addSetting(__language__(50001),'plugin://plugin.audio.skai',10,os.path.join(__settings__.getAddonInfo('path'),'resources','images','settings.png'))
 
 # Show List
 def INDEX1(url,name):
-	req=urllib2.Request(url)
+	req=urllib.request.Request(url)
 	req.add_header('Accept', '*/*')
 	req.add_header('Connection', 'keep-alive')
 	req.add_header('Referer', url)
 	req.add_header('Connection', 'keep-alive')
 	req.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) XBMC Multimedia System')
-	response = urllib2.urlopen(req)
+	response = urllib.request.urlopen(req)
 	link=response.read()
 	response.close()
 	link=normalize_link(link)
-	bblock=re.compile('configuration(.+?)}],').findall(link)
-	ep_image=bblock[0].split('\'')[1]
-	ep_audio_base=bblock[0].split('\'')[5].strip()
-	ep_cur=''
-	if ep_audio_base[-5:-4].isalpha():
-	  ep_audio_url1=ep_audio_base[:-13]
-	  ep_cur=ep_audio_base[-5:-4]
-	else:
-	  ep_audio_url1=ep_audio_base[:-12]
-	ep_audio_url2=ep_audio_base[-4:]
-	#print 'SKAI RADIO ep_title='+name+' ep_image='+ep_image+' ep_audio_url1='+ep_audio_url1+' ep_audio_url2='+ep_audio_url2+' alpha char='+ep_audio_base[-5:-4]
-	block=re.compile('episode_list_dropdown">(.+?)</select>').findall(link)
-	if xbmcplugin.getSetting(int( sys.argv[ 1 ] ),"lazylist") == "true":
-	  armatch=block[0].split('</option>')
-	  for i in range(len(armatch)-1):
-	    ep_date=armatch[i].split('>')
-	    ep_date=ep_date[1].strip()
-	    ep_url=ep_audio_url1+ep_date.split('/')[2]+ep_date.split('/')[1]+ep_date.split('/')[0]+ep_audio_url2
-	    #print 'SKAI RADIO ep_title='+name+' ep_date='+ep_date+' ep_url='+ep_url
-	    addLink(name+' - '+ep_date.split('/')[2]+'/'+ep_date.split('/')[1]+'/'+ep_date.split('/')[0],ep_url,ep_image.split('?')[0])
-	if xbmcplugin.getSetting(int( sys.argv[ 1 ] ),"lazylist") == "false":
-	  episodes=re.compile('data-url(.+?)"(.+?)"(.+?)>(.+?)</option>').findall(block[0])
-	  for buffer1, ep_url, buffer2, ep_date in episodes:
-	    addDir(name+' - '+ep_date.strip(),BaseURL+ep_url,2,os.path.join(__settings__.getAddonInfo('path'),'resources','images','defFolder.png'))
-	if xbmcplugin.getSetting(int( sys.argv[ 1 ] ),"goback") == "true":
-	  addSetting('<< [ Back ]','plugin://plugin.audio.skai/',11,os.path.join(__settings__.getAddonInfo('path'),'resources','images','defFolder.png'))
+	block=re.compile('="selected">(.+?)</select>').findall(link)
+	episodes=re.compile('data-url = "(.+?)">(.+?)</option>').findall(block[0])
+	for ep_url, ep_date in episodes:
+	  addDir(name+' - '+ep_date.strip(),BaseURL+ep_url,2,os.path.join(__settings__.getAddonInfo('path'),'resources','images','defFolder.png'))
+	addSetting('<< [ Back ]','plugin://plugin.audio.skai/',11,os.path.join(__settings__.getAddonInfo('path'),'resources','images','defFolder.png'))
 
 # Non-Lazy Show List
 def INDEX2(url,name):
-	req=urllib2.Request(url)
+	req=urllib.request.Request(url)
 	req.add_header('Accept', '*/*')
 	req.add_header('Connection', 'keep-alive')
 	req.add_header('Referer', url)
 	req.add_header('Connection', 'keep-alive')
 	req.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) XBMC Multimedia System')
-	response = urllib2.urlopen(req)
+	response = urllib.request.urlopen(req)
 	link=response.read()
 	response.close()
 	link=normalize_link(link)
@@ -115,8 +90,7 @@ def INDEX2(url,name):
 	ep_image=block[0].split('\'')[1]
 	ep_audio=block[0].split('\'')[5].strip()
 	addLink(name,ep_audio,ep_image.split('?')[0])
-	if xbmcplugin.getSetting(int( sys.argv[ 1 ] ),"goback") == "true":
-	  addSetting('<< [ Back ]','plugin://plugin.audio.skai/',11,os.path.join(__settings__.getAddonInfo('path'),'resources','images','defFolder.png'))
+	addSetting('<< [ Back ]','plugin://plugin.audio.skai/',11,os.path.join(__settings__.getAddonInfo('path'),'resources','images','defFolder.png'))
 
 def get_params():
         param=[]
@@ -140,56 +114,57 @@ def addLink(name,url,iconimage):
         icoimg = os.path.join(__settings__.getAddonInfo('path'),iconimage)
         if icoimg.count('http:') > 0:
                 icoimg=iconimage
-        liz=xbmcgui.ListItem(name, iconImage="DefaultAudio.png", thumbnailImage=iconimage)
-        liz.setInfo( type="Audio", infoLabels={ "Title": name } )
-        liz.setProperty( "Fanart_Image", fanart )
-        liz.setProperty( "IsPlayable", "true" )
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz,isFolder=False)
+        liz = xbmcgui.ListItem(name)
+        liz.setArt({'icon':icoimg, 'fanart': fanart})
+        vinfo = liz.getVideoInfoTag()
+        vinfo.setTitle(name)
+        vinfo.setMediaType('video')
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
         return ok
 
 def addDir(name,url,mode,iconimage):
-        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+        u=sys.argv[0]+"?url="+urllib.parse.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.parse.quote_plus(name)
         ok=True
         icoimg = os.path.join(__settings__.getAddonInfo('path'),iconimage)
         if icoimg.count('http:') > 0:
                 icoimg=iconimage
-        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=icoimg)
-        liz.setInfo( type="Audio", infoLabels={ "Title": name } )
-        liz.setProperty( "Fanart_Image", fanart )
+        liz = xbmcgui.ListItem(name)
+        liz.setArt({'icon':icoimg, 'fanart': fanart})
+        vinfo = liz.getVideoInfoTag()
+        vinfo.setTitle(name)
+        vinfo.setPath(u)
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
         return ok
 
 def addSetting(name,url,mode,iconimage):
-        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+        u=sys.argv[0]+"?url="+urllib.parse.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.parse.quote_plus(name)
         ok=True
         icoimg = os.path.join(__settings__.getAddonInfo('path'),iconimage)
         if icoimg.count('http:') > 0:
                 icoimg=iconimage
-        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=icoimg)
-        liz.setInfo( type="Audio", infoLabels={ "Title": name } )
-        liz.setProperty( "Fanart_Image", fanart )
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
-        return ok 
+        liz = xbmcgui.ListItem(name)
+        liz.setArt({'icon':icoimg, 'fanart': fanart})
+        vinfo = liz.getVideoInfoTag()
+        vinfo.setTitle(name)
+        vinfo.setMediaType('video')
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)
+        return ok
 
 def normalize_link(link):
-        match=re.compile('charset=(.+?)"').findall(link)
+        decoded = link.decode('utf-8')
+        match=re.compile('charset=(.+?)"').findall(decoded)
         if not match:
                 link=link.replace('\t','').replace('\r\n','').replace('\n','')
                 return link
         elif match[0].upper() == "UTF-8":
-                link=link.replace('\t','').replace('\r\n','').replace('\n','')
+                link=decoded.replace('\t','').replace('\r\n','').replace('\n','')
                 return link
         else:
-                link=link.replace('\t','').replace('\r\n','').replace('\n','').decode(match[0]).encode('utf-8')
-                return link
-
-def LoadSettings():
-        __settings__.openSettings(sys.argv[ 0 ])
-        timeout = int(__settings__.getSetting("socket_timeout"))
-        socket.setdefaulttimeout(timeout)
+                link=decoded.replace('\t','').replace('\r\n','').replace('\n','').encode('utf-8')
+                return link.decode('UTF-8')
 
 def PageBack():
-        xbmc.executebuiltin( "XBMC.Action(Back)" )
+        xbmc.executebuiltin( "Action(Back)" )
 
 params=get_params()
 url=None
@@ -198,11 +173,11 @@ mode=None
 ytid=None
 
 try:
-        url=urllib.unquote_plus(params["url"])
+        url=urllib.parse.unquote_plus(params["url"])
 except:
         pass
 try:
-        name=urllib.unquote_plus(params["name"])
+        name=urllib.parse.unquote_plus(params["name"])
 except:
         pass
 try:
@@ -210,7 +185,7 @@ try:
 except:
         pass
 try:
-        ytid=urllib.unquote_plus(params["ytid"])
+        ytid=urllib.parse.unquote_plus(params["ytid"])
 except:
         pass
 
@@ -221,7 +196,7 @@ except:
 #print "YTid: "+str(ytid)
 
 if mode==None or url==None or len(url)<1:
-        INDEX(url)
+    INDEX(url)
 elif mode==1:
 	INDEX1(url,name)
 elif mode==2:
